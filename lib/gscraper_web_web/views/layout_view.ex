ba917babab9ce9d7af5a373599f28046b76ee293
Long Nguyen <nguyenduclong@msn.com>
2021-04-14 14:29:21 +0700

    [#4] Generate the CSS classes for body

--- lib/gscraper_web_web/views/layout_view.ex
+++ lib/gscraper_web_web/views/layout_view.ex
@@ -1,3 +1,13 @@
 defmodule GscraperWebWeb.LayoutView do
   use GscraperWebWeb, :view
+
+  def body_class_name(conn) do
+    "#{module_class_name(conn)} #{action_name(conn)}"
+  end
+
+  def module_class_name(conn) do
+    controller_module(conn)
+    |> Phoenix.Naming.resource_name("Controller")
+    |> String.replace("_", "-")
+  end
 end
