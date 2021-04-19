df9e0cc3097a678cf1d3815b7f8d5909ff567f08
Long Nguyen <nguyenduclong@msn.com>
2021-04-19 15:44:50 +0700

    [#5] Correct the home page path

--- test/gscraper_web/features/home_page/view_home_page_test.exs
+++ test/gscraper_web/features/home_page/view_home_page_test.exs
@@ -2,7 +2,7 @@ defmodule GscraperWeb.HomePage.ViewHomePageTest do
   use GscraperWeb.FeatureCase
 
   feature "view home page", %{session: session} do
-    visit(session, Routes.page_path(GscraperWeb.Endpoint, :index))
+    visit(session, Routes.dashboard_path(GscraperWeb.Endpoint, :index))
 
     assert_has(session, Query.text("Welcome to Phoenix!"))
   end
