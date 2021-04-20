36213cf821f675f2f1d11ccd954a4f3e5bd569ad
Long Nguyen <nguyenduclong@msn.com>
2021-04-20 14:40:41 +0700

    [#7] Add the method to get a user by ID

--- lib/gscraper/account/users.ex
+++ lib/gscraper/account/users.ex
@@ -8,6 +8,21 @@ defmodule Gscraper.Account.Users do
   alias Gscraper.Repo
 
   @doc """
+  Gets a single user.
+
+  Raises `Ecto.NoResultsError` if the User does not exist.
+
+  ## Examples
+
+      iex> get_user!(123)
+      %User{}
+      iex> get_user!(456)
+      ** (Ecto.NoResultsError)
+
+  """
+  def get_user!(id), do: Repo.get!(User, id)
+
+  @doc """
   Creates a user.
 
   ## Examples
