56e9592cd7616ce6cba3f42d464e52f9ffe240d3
Long Nguyen <nguyenduclong@msn.com>
2021-04-20 08:42:48 +0700

    [#5] Extract the validation messages

--- lib/gscraper/accounts/user.ex
+++ lib/gscraper/accounts/user.ex
@@ -1,6 +1,7 @@
 defmodule Gscraper.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
+  import GscraperWeb.Gettext
 
   alias Gscraper.Accounts.Password
 
@@ -28,9 +29,9 @@ defmodule Gscraper.Accounts.User do
   def validate_password(changeset) do
     changeset
     |> validate_length(:password, min: 6)
-    |> validate_format(:password, ~r/[0-9]+/, message: "Password must contain a number")
-    |> validate_format(:password, ~r/[A-Z]+/, message: "Password must contain an upper-case letter")
-    |> validate_format(:password, ~r/[a-z]+/, message: "Password must contain a lower-case letter")
+    |> validate_format(:password, ~r/[0-9]+/, message: dgettext("auth", "Password must contain a number"))
+    |> validate_format(:password, ~r/[A-Z]+/, message: dgettext("auth", "Password must contain an upper-case letter"))
+    |> validate_format(:password, ~r/[a-z]+/, message: dgettext("auth", "Password must contain a lower-case letter"))
     |> validate_confirmation(:password)
     |> put_password_hash
   end
