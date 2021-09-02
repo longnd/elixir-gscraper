996a451773010e5ea9e7ffa8bff346d5fa829ec8
Long Nguyen <nguyenduclong@msn.com>
2021-09-02 22:49:01 +0700

    [#7] Update dependencies

--- mix.exs
+++ mix.exs
@@ -51,18 +51,17 @@ defmodule Gscraper.MixProject do
       {:dialyxir, "~> 1.1.0", [only: [:dev], runtime: false]},
       {:credo, "~> 1.5.5", [only: [:dev, :test], runtime: false]},
       {:phoenix, "~> 1.5.12"},
-      {:phoenix_ecto, "~> 4.1"},
-      {:ecto_sql, "~> 3.4"},
+      {:phoenix_ecto, "~> 4.4"},
+      {:ecto_sql, "~> 3.7"},
       {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 2.11"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_dashboard, "~> 0.4"},
+      {:phoenix_html, "~> 3.0"},
+      {:phoenix_live_reload, "~> 1.3", only: :dev},
+      {:phoenix_live_dashboard, "~> 0.5"},
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"},
-      {:nimble_phx_gen_template, "~> 2.2.0", only: :dev, runtime: false}
+      {:plug_cowboy, "~> 2.5"}
     ]
   end
 
