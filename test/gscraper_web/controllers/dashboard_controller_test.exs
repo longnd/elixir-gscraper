21ba414eb01d4a545d08838f2e4326cb02a2ac98
Long Nguyen <nguyenduclong@msn.com>
2021-09-11 23:24:12 +0700

    [#7] Add an additional test case for the dashboard controller

--- test/gscraper_web/controllers/dashboard_controller_test.exs
+++ test/gscraper_web/controllers/dashboard_controller_test.exs
@@ -2,13 +2,20 @@ defmodule GscraperWeb.DashboardControllerTest do
   use GscraperWeb.ConnCase, async: true
 
   describe "GET /" do
-    test "renders the dashboard page", %{conn: conn} do
+    test "renders the dashboard page given the user already logged in", %{conn: conn} do
       conn =
         conn
         |> login_user
-        |> get("/")
+        |> get(Routes.dashboard_path(conn, :index))
 
       assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    end
+
+    test "redirects to the log in page given the user has not logged in", %{conn: conn} do
+      conn = get(conn, Routes.dashboard_path(conn, :index))
+
+      assert redirected_to(conn) == Routes.session_path(conn, :new)
+      assert get_flash(conn, :error) == dgettext("auth", "Authentication required.")
     end
   end
 end
